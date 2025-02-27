dcb7cf7893c7b217affe6115286f5fd4bc300a0c
MaskedCritic <mr.waite@gmail.com>
2025-02-27 11:56:16 -0800

    Snap-to functionality
    - added card slot
    - added card snapping to card slot

--- Card.gd
+++ Card.gd
@@ -3,6 +3,9 @@ extends Node2D
 signal hovered
 signal hovered_off
 
+var is_in_slot = false
+var slot_card_is_in = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_parent().connect_card_signals(self)
--- CardManager.gd
+++ CardManager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
-
+const COLLISION_MASK_CARD_SLOT = 2
 var screen_size
 var card_being_dragged
 var is_hovering_on_card
@@ -9,20 +9,21 @@ var is_hovering_on_card
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			var card = check_for_card()
+			var card = check_for_card(COLLISION_MASK_CARD)
 			if card:
 				#game logic required here to allow snap back
 				start_drag(card)
 		else:
 			#game logic required here to snap back to old position if invalid placement
-			stop_drag()
+			if (card_being_dragged):
+				stop_drag()
 
-func check_for_card():
+func check_for_card(card_type):
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
 	parameters.position = get_global_mouse_position()
 	parameters.collide_with_areas = true;
-	parameters.collision_mask = COLLISION_MASK_CARD;
+	parameters.collision_mask = card_type;
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
 		return get_card_with_highest_z_index(result)
@@ -52,7 +53,7 @@ func on_hovered_on_card(card):
 func on_hovered_off_card(card):
 	if (!card_being_dragged):
 		highlight_card(card, false)
-		var new_card_hovered = check_for_card()
+		var new_card_hovered = check_for_card(COLLISION_MASK_CARD)
 		if (new_card_hovered):
 			highlight_card(new_card_hovered, true)
 		else:
@@ -69,9 +70,20 @@ func highlight_card(card, hovered):
 func start_drag(card):
 	card_being_dragged = card
 	card.scale = Vector2(1, 1)
+	if (card_being_dragged.is_in_slot):
+		card_being_dragged.is_in_slot = false
+		card_being_dragged.slot_card_is_in.card_in_slot = false
+		card_being_dragged.slot_card_is_in = null
+	
 
 func stop_drag():
 	card_being_dragged.scale = Vector2(1.05, 1.05)
+	var card_slot_found = check_for_card(COLLISION_MASK_CARD_SLOT)
+	if (card_slot_found && !card_slot_found.card_in_slot):
+		card_being_dragged.position = card_slot_found.position
+		card_being_dragged.is_in_slot = true
+		card_being_dragged.slot_card_is_in = card_slot_found
+		card_slot_found.card_in_slot = true
 	card_being_dragged = null
 
 # Called when the node enters the scene tree for the first time.
